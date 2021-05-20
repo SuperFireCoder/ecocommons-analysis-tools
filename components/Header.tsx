942b80f07ef10dfc9b75836f61d90105144b2c04
James Li <j.li7@griffith.edu.au>
2021-05-20 16:40:30 +1000

    Updated header to pass URLs from `process.env` directly

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,30 @@ import SignInOutButton from "./SignInOutButton";
 
 // This <Header /> injects the <SignInOutButton /> specific to this site
 
-export default function Header(props: ComponentProps<typeof EcHeader>) {
-    return <EcHeader signInOutButton={<SignInOutButton />} {...props} />;
+export default function Header(
+    props: Omit<ComponentProps<typeof EcHeader>, "tabLinks">
+) {
+    return (
+        <EcHeader
+            signInOutButton={<SignInOutButton />}
+            tabLinks={{
+                ECOCOMMONS_ROOT:
+                    process.env.NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_ROOT ??
+                    "#",
+                ECOCOMMONS_DATA_VISUALISATIONS:
+                    process.env
+                        .NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_DATA_VISUALISATIONS ??
+                    "#",
+                ECOCOMMONS_TOOLS_FUNCTIONS:
+                    process.env
+                        .NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_TOOLS_FUNCTIONS ??
+                    "#",
+                ECOCOMMONS_VIRTUAL_LABORATORIES:
+                    process.env
+                        .NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_VIRTUAL_LABORATORIES ??
+                    "#",
+            }}
+            {...props}
+        />
+    );
 }
