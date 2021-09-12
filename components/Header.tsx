4c02bf79287ecda7b5a45b3f85d8ca453822f215
Rob Cosgrove <r.cosgrove@griffith.edu.au>
2021-09-13 09:48:57 +1000

    switch env to publicRuntimeConfig

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,9 @@
 import { ComponentProps } from "react";
 import { Header as EcHeader } from "@ecocommons-australia/ui-library";
 import SignInOutButton from "./SignInOutButton";
+import getConfig from 'next/config'
+
+const config = getConfig()
 
 // This <Header /> injects the <SignInOutButton /> specific to this site
 
@@ -29,18 +32,18 @@ export default function Header({
             signInOutButton={<SignInOutButton />}
             tabLinks={{
                 ECOCOMMONS_ROOT:
-                    process.env.NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_ROOT ??
+                    config.publicRuntimeConfig.NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_ROOT ??
                     "#",
                 ECOCOMMONS_DATA_VISUALISATIONS:
-                    process.env
+                    config.publicRuntimeConfig
                         .NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_DATA_VISUALISATIONS ??
                     "#",
                 ECOCOMMONS_TOOLS_FUNCTIONS:
-                    process.env
+                    config.publicRuntimeConfig
                         .NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_TOOLS_FUNCTIONS ??
                     "/",
                 ECOCOMMONS_VIRTUAL_LABORATORIES:
-                    process.env
+                    config.publicRuntimeConfig
                         .NEXT_PUBLIC_UI_LIBRARY_HEADER_ECOCOMMONS_VIRTUAL_LABORATORIES ??
                     "#",
             }}
--- util/env.ts
+++ util/env.ts
@@ -1,17 +1,21 @@
+import getConfig from 'next/config'
+
+const config = getConfig()
+
 export function getAnalysisPlaygroundBackendServerUrl() {
-    return process.env
+    return config.publicRuntimeConfig
         .NEXT_PUBLIC_ANALYSIS_PLAYGROUND_BACKEND_SERVER_URL as string;
 }
 
 export function getJupyterhubServerUrl() {
-    return process.env
+    return config.publicRuntimeConfig
         .NEXT_PUBLIC_JUPYTERHUB_SERVER_URL as string;
 }
 
 export function getKeycloakAuthParameters() {
     return {
-        url: process.env.NEXT_PUBLIC_KEYCLOAK_AUTH_URL as string,
-        realm: process.env.NEXT_PUBLIC_KEYCLOAK_AUTH_REALM as string,
-        clientId: process.env.NEXT_PUBLIC_KEYCLOAK_AUTH_CLIENT_ID as string,
+        url: config.publicRuntimeConfig.NEXT_PUBLIC_KEYCLOAK_AUTH_URL as string,
+        realm: config.publicRuntimeConfig.NEXT_PUBLIC_KEYCLOAK_AUTH_REALM as string,
+        clientId: config.publicRuntimeConfig.NEXT_PUBLIC_KEYCLOAK_AUTH_CLIENT_ID as string,
     };
 }
